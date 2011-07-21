6717052d7018c841073eb4800ab470ddf0fce48d
Brian Rowe <rowe@muxspace.com>
2011-07-21 14:19:51 -0400

    Cleaned up stringifying erlang terms to view in the browser

---
+++ erlang/erl_viewer.erl
@@ -0,0 +1,19 @@
+%% This module formats erlang terms to a human-readable string
+-module(erl_viewer).
+-export([to_string/3, to_string/1]).
+
+%% Client:mapred([{<<"test_2">>,<<"1">>}], 
+%%   [{map, {modfun, erl_viewer,to_string}, none, true}]).
+to_string(RiakObject, _, _) ->
+  O = riak_object:get_value(RiakObject),
+  %io:format("[erl_viewer] Converting object~n  ~p~n", [O]),
+  to_string(O).
+
+to_string(<<"{}">>) -> [];
+to_string({M,S,N}) -> [M,S,N];
+to_string(Term) when is_binary(Term) -> to_string(binary_to_term(Term));
+to_string(Term) ->
+  Out = lists:flatten(io_lib:format("~p", [Term])),
+  io:format("[erl_viewer] Flattened to~n  ~p~n", [Out]),
+  [ list_to_binary(Out) ].
+
